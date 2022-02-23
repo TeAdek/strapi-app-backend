fb4b6c7b85bdf0bc5b3f936c0c5c30221d0025c8
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-22 20:15:41 -0800

    Add related data to Home and Review detail page

--- taiwo-backandfrontend/frontend/src/pages/Homepage.js
+++ taiwo-backandfrontend/frontend/src/pages/Homepage.js
@@ -11,6 +11,14 @@ query GetReviews {
         title,
         rating,
         body
+        categories {
+          data {
+            id
+            attributes {
+              name
+            }
+          }
+        }
       }
     }
   }
@@ -32,8 +40,9 @@ console.log(data)
           <div className="rating">{review.attributes.rating}</div>
           <h2>{review.attributes.title}</h2>
 
-          <small>console list</small>
-
+          {review.attributes.categories.data.map(c => (
+  <small key={c.id}>{c.attributes.name}</small>
+))}
           <p>{review.attributes.body.substring(0, 200)}...</p>
           <Link to={`/details/${review.id}`}>Read more</Link>
         </div>
--- taiwo-backandfrontend/frontend/src/pages/ReviewDetails.js
+++ taiwo-backandfrontend/frontend/src/pages/ReviewDetails.js
@@ -1,41 +1,50 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 import { useQuery, gql } from "@apollo/client";
 
 const REVIEW = gql`
-query getPageById($reviewId: ID){
-  review(id: $reviewId) {
-    data {
-      id
-      attributes {
-        body
-        title,
-        rating,
+  query getPageById($reviewId: ID) {
+    review(id: $reviewId) {
+      data {
+        id
+        attributes {
+          body
+          title
+          rating
+          categories {
+            data {
+              id
+              attributes {
+                name
+              }
+            }
+          }
+        }
       }
     }
   }
-}
-`
+`;
 export default function ReviewDetails() {
-  const {id} = useParams()
-  const {loading, error, data} = useQuery(REVIEW, {
-    variables: {reviewId: id}
-  })
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(REVIEW, {
+    variables: { reviewId: id },
+  });
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error ;(</p>;
 
-  console.log(data)
+  console.log(data);
 
   return (
-      <div className="review-card">
-        <div className="rating">{data.review.data.attributes.rating}</div>
-        <h2>{data.review.data.attributes.title}</h2>
+    <div className="review-card">
+      <div className="rating">{data.review.data.attributes.rating}</div>
+      <h2>{data.review.data.attributes.title}</h2>
 
-        <small>console list</small>
+      {data.review.data.attributes.categories.data.map((c) => (
+        <small key={c.id}>{c.attributes.name}</small>
+      ))}
 
-        <p>{data.review.data.attributes.body}</p>
-      </div>
-    
-  )
+      <p>{data.review.data.attributes.body}</p>
+    </div>
+  );
 }
