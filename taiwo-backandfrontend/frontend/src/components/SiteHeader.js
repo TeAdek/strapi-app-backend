48d393b17a46ea87dc3215363fc9cb27ac5a5d7d
TeAdek <71227081+TeAdek@users.noreply.github.com>
2022-02-22 18:35:56 -0800

    Create relational data
    Content type relating to the review content type

--- taiwo-backandfrontend/frontend/src/components/SiteHeader.js
+++ taiwo-backandfrontend/frontend/src/components/SiteHeader.js
@@ -1,10 +1,36 @@
  import React from 'react'
  import { Link } from 'react-router-dom'
+ import { useQuery, gql } from "@apollo/client";
 
+const CATEGORIES = gql`
+query Category {
+  categories {
+    data {
+      id
+      attributes {
+        name
+      }
+    }
+  }
+}
+`
  export default function SiteHeader() {
+   const {loading, error, data} = useQuery(CATEGORIES)
+
+   if(loading) return <p>Loading categories...</p>
+   if(error) return <p>Error fetching categories</p>
+console.log(data)
    return (
      <div className='site-header'>
          <Link to="/"><h1>Ninja Reviews</h1></Link>
+         <nav className='categories'>
+           <span>Filter reviews by category:</span>
+           {data.categories.data.map(category =>(
+             <Link key={category.id} to={`/category/${category.id}`}>
+               {category.attributes.name}
+             </Link>
+           ))}
+         </nav>
      </div>
    )
  }
